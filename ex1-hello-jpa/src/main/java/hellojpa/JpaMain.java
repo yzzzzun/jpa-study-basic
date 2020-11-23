b451d9c05b3b231401c5f30b9fe4093f41a52409
youngjun.jin <yzzzzun@gmail.com>
2020-11-23 20:07:29 +0900

    jpa 프로젝트 생성  - .gitignore 추가  - h2 database 연결 및 Member테이블에 데이터 추가  - jpa 기본 추가 로직 예제 구현

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,31 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+ public class JpaMain {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityMangerFactory = Persistence.createEntityManagerFactory("hello");
+		EntityManager entityManager = entityMangerFactory.createEntityManager();
+
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+
+		try {
+			Member member = new Member();
+			member.setId(1L);
+			member.setName("HelloA");
+			entityManager.persist(member);
+
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+		}finally {
+			entityManager.close();
+		}		
+		entityMangerFactory.close();
+	}
+}
