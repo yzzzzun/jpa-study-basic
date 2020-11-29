78b475f3f8c6e8b66b4254d54c0c7d2f45945428
youngjun.jin <yzzzzun@gmail.com>
2020-11-30 01:17:55 +0900

    entity매핑 및 id의 영속성 컨텍스트에서 수행 로직확인

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
- public class JpaMain {
+public class JpaMain {
 
 	public static void main(String[] args) {
 		EntityManagerFactory entityMangerFactory = Persistence.createEntityManagerFactory("hello");
@@ -16,16 +16,20 @@ import javax.persistence.Persistence;
 
 		try {
 			Member member = new Member();
-			member.setId(1L);
-			member.setName("HelloA");
+			member.setUsername("test");
+
+			// id 는 db에 들어가야 알 수 있어 insert를 미리 쳐준다
+			System.out.println("================");
 			entityManager.persist(member);
+			System.out.println("member id : " + member.getId());
+			System.out.println("================");
 
 			transaction.commit();
 		} catch (Exception e) {
 			transaction.rollback();
-		}finally {
+		} finally {
 			entityManager.close();
-		}		
+		}
 		entityMangerFactory.close();
 	}
 }
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,15 +1,43 @@
 package hellojpa;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Member {
 
+	// ID 기본 키 매핑 방법
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String name;
+	@Column(name = "name", nullable = false)
+	private String username;
+
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	// LocalDateTime 을 사용하면 생략 가능
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	@Lob
+	private String description;
 
 	public Long getId() {
 		return id;
@@ -19,12 +47,52 @@ public class Member {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public RoleType getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(RoleType roleType) {
+		this.roleType = roleType;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 }
