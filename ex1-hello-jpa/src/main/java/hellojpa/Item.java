697853d93d789c5a4c353c2aeee8610cdca8e81a
youngjun.jin <yzzzzun@gmail.com>
2020-12-12 18:45:21 +0900

    상속관계 매핑 - 구현 클래스별 테이블 전략 abstract 클래스로 만들어야 테이블이 생성되지 않음 단점은 부모 클래스타입으로 조회하면 union 쿼리로 모든 테이블을 다뒤져서 찾아내기때문에 비효율적

--- ex1-hello-jpa/src/main/java/hellojpa/Item.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -8,9 +8,9 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn
-public class Item {
+public abstract class Item {
 
 	@Id
 	@GeneratedValue
