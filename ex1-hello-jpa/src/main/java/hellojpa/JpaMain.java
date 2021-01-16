41d50b1ddcccc5d29b1eec6a2c8a270405455038
yzzzzun <yzzzzun@gmail.com>
2021-01-17 00:53:14 +0900

    값 타입 컬렉션 조회 예제 - 지연로딩 전략 체크

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,8 @@
 package hellojpa;
 
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -20,15 +23,34 @@ public class JpaMain {
 			Member member = new Member();
 			member.setUsername("member1");
 			member.setHomeAddress(homeAddress);
-			
+
 			member.getFavoriteFoods().add("치킨");
 			member.getFavoriteFoods().add("족발");
 			member.getFavoriteFoods().add("피자");
-			
+
 			member.getAddressHistory().add(new Address("old1", "street1", "1111"));
 			member.getAddressHistory().add(new Address("old2", "street2", "2222"));
 
 			entityManager.persist(member);
+
+			entityManager.flush();
+			entityManager.clear();
+
+			System.out.println("----------------------START---------------");
+			Member findMember = entityManager.find(Member.class, member.getId());
+
+			//LAZY Loading 확인 
+			List<Address> addressHistory = findMember.getAddressHistory();
+			for (Address address : addressHistory) {
+
+				System.out.println("address = " + address.getCity());
+			}
+
+			Set<String> favoriteFoods = findMember.getFavoriteFoods();
+			for (String food : favoriteFoods) {
+
+				System.out.println("favorite food = " + food);
+			}
 
 			transaction.commit();
 		} catch (Exception e) {
